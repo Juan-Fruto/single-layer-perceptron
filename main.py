5c05de8fc1045ab06e5e0a019ffe578f3d0a5b12
juan-fruto <jfruto@ucol.mx>
2023-08-20 23:36:48 -0600

    add dataset_parsing func, iris func & bill_authentication func in main.py

--- main.py
+++ main.py
@@ -1,28 +1,61 @@
 from perceptrons import SingleLayerPerceptron 
 import csv
 
-ALFA = 0.5
+def dataset_parsing(csv_filename):
+  with open(csv_filename) as f:
+    reader = csv.DictReader(f)
+    
+    data = []
+
+    for row in reader:
+      num_type_row =  {key: float(value) for key, value in row.items()}
+      data.append(num_type_row)
+
+    return data
+   
+def iris():
+  dataset = dataset_parsing("./datasets/iris/Iris.csv")
+  
+  # removing the id field
+  iris_dataframe = []
+  for i in dataset:
+    del i['Id']
+    iris_dataframe.append(i)
+    pass
+
+  slp_iris = SingleLayerPerceptron()
+
+  # training the dataframe
+  slp_iris.set_dataset(iris_dataframe, .8).train()
+
+  # showing the model
+  print("Model: ", slp_iris.model)
+
+  # show the predictions (outputs)
+  print("Predictions", slp_iris.predict())
+
+def bill_autentication():
+  ba_dataframe = dataset_parsing("./datasets/bill_authentication/bill_authentication.csv")
+
+  slp_iris = SingleLayerPerceptron()
+
+  # training the dataframe
+  slp_iris.set_dataset(ba_dataframe, .8).train()
+  
+  # print("--> Debugin test <--")
+  # print("Train dataset:", slp_iris._train_dataset)
+  # print("Test dataset:", slp_iris._test_dataset)
+  # print("--> <--")
+
+  # showing the model
+  print("Model: ", slp_iris.model)
+
+  # show the predictions (outputs)
+  print("Predictions", slp_iris.predict())
 
 def main():
-  slp = SingleLayerPerceptron()
-
-  slp\
-    .set_dataset([
-      {'x1': 1,'x2': 10,'output': -1},
-      {'x1': 5,'x2': 2,'output': 1},
-      {'x1': 7,'x2': 3,'output': 1},
-      {'x1': 3,'x2': 7,'output': -1},
-      {'x1': 3.5,'x2': 9,'output': -1},
-    ], .8)\
-    .train()
-  
-  print("--> Debugin test <--")
-  print("Train dataset:", slp._train_dataset)
-  print("Test dataset:", slp._test_dataset)
-  print("--> <--")
-
-  print("Model: ", slp.model)
-  print("Predictions", slp.predict())
+  iris()
+  bill_autentication()
 
 if __name__ == "__main__":
   main()
